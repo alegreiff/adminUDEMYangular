99a6cba1954090df67d2b842046e1abc0a0b5d22
Jaime de Greiff <alegreiff@gmail.com>
2020-06-02 18:51:12 -0500

    02 de junio y avanzando- Sección 8 finish

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd, Data } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,17 +12,32 @@ import { Observable } from 'rxjs';
 })
 export class BreadcrumbsComponent implements OnInit {
 titulo: string;
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private title: Title,
+    private meta: Meta
+    ) {
+    this.getDataRoute()
+    .subscribe(data => {
+      this.titulo = data.titulo;
+      this.title.setTitle(data.titulo);
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo + ' de la serie de creaciones angular de Jaime de Greiff'
+      };
+      this.meta.updateTag ( metaTag )
+    })
+   }
 
   ngOnInit(): void {
-    this.getDataRoute()
+
   }
 
   getDataRoute(){
-    this.router.events.pipe(
+    return this.router.events.pipe(
       filter(evento => evento instanceof ActivationEnd),
       filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
       map( (evento: ActivationEnd) => evento.snapshot.data)
-    ).subscribe(data => this.titulo = data.titulo)
+    )
   }
 }
